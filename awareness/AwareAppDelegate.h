2e8466e1f5dc29ff96c316a274b9c51daadb8f59
Dawa Sherpa <dsherpa@etsy.com>
2013-12-05 11:13:31 -0500

    Very first commit. Empty project

---
+++ awareness/AwareAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AwareAppDelegate.h
+//  awareness
+//
+//  Created by Dawa Lama on 12/5/13.
+//  Copyright (c) 2013 Dawa Lama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AwareAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ awareness/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  awareness
+//
+//  Created by Dawa Lama on 12/5/13.
+//  Copyright (c) 2013 Dawa Lama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AwareAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AwareAppDelegate class]));
+    }
+}
